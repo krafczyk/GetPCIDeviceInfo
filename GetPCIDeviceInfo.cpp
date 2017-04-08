f8050b31ba2e95096c446ddade6386442baeb35a
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2017-04-08 11:09:47 -0500

    Use ArgParse for argument parsing.

--- GetPCIDeviceInfo.cpp
+++ GetPCIDeviceInfo.cpp
@@ -1,6 +1,21 @@
-#include <stdio.h>
+#include <cstdio>
+#include <string>
+
+#include "ArgParse/ArgParse.h"
+
 #include "pci/pci.h"
 
 int main(int argc, char** argv) {
+	std::string directory = "";
+
+	ArgParse::ArgParser Parser("Program to print PCI Device information inside a directory");
+	Parser.AddArgument("-d/--directory", "The directory from which to get PCI information", &directory, ArgParse::Argument::Required);
+	if (Parser.ParseArgs(argc, argv) < 0) {
+		fprintf(stderr, "There was a problem parsing the arguments!");
+		return -1;
+	}
+
+	printf("The directory passed was: (%s)\n", directory.c_str());
+
 	return 0;
 }
